eea9465158537281bded2d587ae6e391e8bb6c9f
Giusseppe PC <giudelvecchio@gmail.com>
2020-07-14 22:51:07 -0400

    complete

--- freeCodeCamp/destroyer.js
+++ freeCodeCamp/destroyer.js
@@ -1,8 +1,10 @@
 function destroyer(arr) {
-    return arr
+    let args = Array.from(arguments).slice(1);
+    let filteredArr = arr.filter((val) => !args.includes(val)); 
+    return filteredArr
 }
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3) , [1, 1])
-console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) , [1, 5, 1])
-console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5) , [1])
-console.log(destroyer([2, 3, 2, 3], 2, 3) , [])
-console.log(destroyer(["tree", "hamburger", 53], "tree", 53) , ["hamburger"])
+// console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) , [1, 5, 1])
+// console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5) , [1])
+// console.log(destroyer([2, 3, 2, 3], 2, 3) , [])
+// console.log(destroyer(["tree", "hamburger", 53], "tree", 53) , ["hamburger"])
